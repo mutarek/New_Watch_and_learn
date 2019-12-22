f588146b3ef3be793720672346dc885e3196b90b
mohiuddintarek <41743928+mohiuddin-Tarek@users.noreply.github.com>
2019-12-22 11:39:43 +0600

    new

---
+++ app/src/main/java/com/bdsoftwaresolution/watchtolearn/Models/VideoModel.java
@@ -0,0 +1,29 @@
+package com.bdsoftwaresolution.watchtolearn.Models;
+
+public class VideoModel {
+    private String videoTitle,videoUrl,videoDesc;
+
+    public String getVideoTitle() {
+        return videoTitle;
+    }
+
+    public void setVideoTitle(String videoTitle) {
+        this.videoTitle = videoTitle;
+    }
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+
+    public String getVideoDesc() {
+        return videoDesc;
+    }
+
+    public void setVideoDesc(String videoDesc) {
+        this.videoDesc = videoDesc;
+    }
+}
