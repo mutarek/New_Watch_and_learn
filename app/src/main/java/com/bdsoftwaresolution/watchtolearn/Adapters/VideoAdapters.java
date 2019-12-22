f94750a7f2d929b2d55092a7f417654349528ccd
mohiuddintarek <41743928+mohiuddin-Tarek@users.noreply.github.com>
2019-12-22 11:53:59 +0600

    new

---
+++ app/src/main/java/com/bdsoftwaresolution/watchtolearn/Adapters/VideoAdapters.java
@@ -0,0 +1,32 @@
+package com.bdsoftwaresolution.watchtolearn.Adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class VideoAdapters extends RecyclerView.Adapter<VideoAdapters.VideoHolder> {
+    @NonNull
+    @Override
+    public VideoHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull VideoHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class VideoHolder extends RecyclerView.ViewHolder {
+
+        public VideoHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
